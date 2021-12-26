0295bc7ef9f33c77dfae624c2e0e82ee951386ce
Catherine_AMF <catherineamf@gmail.com>
2021-12-26 11:02:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const students = [
+  {
+    id: 10,
+    name: 'John Smith',
+    marks: [10, 8, 6, 9, 8, 7]
+  },
+  {
+    id: 11,
+    name: 'John Doe',
+    marks: [9, 8, 7, 6, 7]
+  },
+  {
+    id: 12,
+    name: 'Thomas Anderson',
+    marks: [6, 7, 10, 8]
+  },
+  {
+    id: 13,
+    name: 'Jean-Baptiste Emanuel Zorg',
+    marks: [10, 9, 8, 9]
+  }
+];
+
+console.log(averageStudentMark(10));
+console.log(averageGroupMark(students));
+
+function averageStudentMark(id) {
+  const student = students.find((item) => item.id == id);
+
+  if (!student) {
+
+    return;
+  }
+
+  const averageMark = getAverageMark(student.marks);
+
+  return averageMark;
+}
+
+function averageGroupMark(students) {
+  const marks = students.reduce((previous, current) => [...previous, ...current.marks], []);
+  const averageMark = getAverageMark(marks);
+
+  return averageMark;
+
+}
+
+function getMarkSum(marks) {
+
+  return marks.reduce((previousValue, currentValue) => previousValue + currentValue);
+}
+
+function getAverageMark(marks) {
+  const markSum = getMarkSum(marks);
+  const averageMark = markSum / marks.length;
+
+  return averageMark;
+}
